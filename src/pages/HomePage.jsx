23e0ac26e377d70024d35c06401ee973e9ec1ef4
levis-creator <47003569+levis-creator@users.noreply.github.com>
2024-08-14 04:58:00 -0700

    Update home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,11 @@
 function HomePage() {
-  return <div> Test home</div>;
+  return (
+    <div>
+      <div className="flex items-center justify-center h-screen bg-white">
+        <h1 className="text-3xl font-bold">Welcome to my page</h1>
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
